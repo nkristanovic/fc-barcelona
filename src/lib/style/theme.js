395f74f1425f7d4e1d11abba278c4e09e640e447
Nikola Kristanović <nikola.kristanovic@gmail.com>
2022-03-25 21:21:49 +0100

    created react app

---
+++ src/lib/style/theme.js
@@ -0,0 +1,16 @@
+export const breakpoints = {
+    mobileLarge: 'min-width: 576px',
+    tablet: 'min-width: 768px',
+    desktop: 'min-width: 1024px',
+    desktopLarge: 'min-width: 1300px'
+};
+
+export const colors = {
+    white: '#ffffff',
+    black: '#000000',
+    goldenYellow: '#EDBB00',
+    mirage: '#181733',
+    midnightBlue: '#003568',
+    mediumDarkPink: '#A50044',
+    paprika: '#99003F'
+}
